00fc6933548b00e74a7d17b34f29ebc135e23f85
li0n0k <lion_ok@mail.ru>
2021-01-15 18:37:06 +0300

    Refactoring

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,11 +1,31 @@
 """Game brain calc logic."""
 
 from operator import add, mul, sub
-from random import randint
+from random import choice, randint
 
 DESCRIPTION = 'What is the result of the expression?'
 GAME_RANGE = (0, 30)
-OPERATIONS = {'+': add, '-': sub, '*': mul}
+OPERATIONS = '+-*'
+
+
+def get_correct_answer(operand1: str, operand2: str, operation: str) -> int:
+    """
+    Calculate correct answer.
+
+    Args:
+        operand1: operand 1
+        operand2: operand 2
+        operation: operation on operands
+
+    Returns:
+        int
+    """
+    if operation == '+':
+        return add(operand1, operand2)
+    elif operation == '-':
+        return sub(operand1, operand2)
+    elif operation == '*':
+        return mul(operand1, operand2)
 
 
 def get_question_and_correct_answer() -> tuple:
@@ -16,11 +36,11 @@ def get_question_and_correct_answer() -> tuple:
         tuple: contains question and correct answer
     """
     operand1, operand2 = [randint(*GAME_RANGE) for _ in range(2)]
-    current_operation = list(OPERATIONS)[randint(0, len(OPERATIONS) - 1)]
-    answer = OPERATIONS[current_operation](operand1, operand2)
+    current_operation = choice(OPERATIONS)
+    answer = str(get_correct_answer(operand1, operand2, current_operation))
     question = '{value1} {operation} {value2}'.format(
         value1=operand1,
         operation=current_operation,
         value2=operand2,
     )
-    return question, str(answer)
+    return question, answer
